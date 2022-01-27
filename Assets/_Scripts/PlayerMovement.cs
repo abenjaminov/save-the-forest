414ee0eb24b9748e04072b1a11e022ca9b3a9f4d
Kremzon <Nadavbeker@gmail.com>
2022-01-27 21:53:55 +0200

    Added move

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,9 +1,34 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Scripts
 {
     public class PlayerMovement : MonoBehaviour
     {
-        
+        [SerializeField] float moveSpeed = 5f;
+        Vector2 playerInput;
+        CharacterController characterController;
+
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            Vector3 positionDelta = new Vector3(playerInput.x, 0, playerInput.y) * moveSpeed * Time.deltaTime;
+            characterController.Move(positionDelta);
+        }
+
+        void OnMove(InputValue inputValue)
+        {
+            playerInput = inputValue.Get<Vector2>();
+            
+        }
     }
 }
